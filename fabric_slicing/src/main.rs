0486d45586f07603031bb8316defe4dd35788403
df5602 <dominik@fankhausers.ch>
2018-12-03 06:24:37 +0100

    setup day 3

---
+++ fabric_slicing/src/main.rs
@@ -0,0 +1,23 @@
+extern crate util;
+
+use std::env;
+
+use util::input::{FileReader, FromFile};
+
+fn main() {
+    let input_file = match env::args().nth(1) {
+        Some(input_file) => input_file,
+        None => {
+            println!("Please supply input file!");
+            std::process::exit(1);
+        }
+    };
+
+    let input: Vec<i64> = match FileReader::read_from_file(input_file) {
+        Ok(input) => input,
+        Err(e) => {
+            println!("Error reading input: {}", e);
+            std::process::exit(1);
+        }
+    };
+}
