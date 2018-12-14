e6479b6d06919242996e461775129c4f1b53094f
df5602 <dominik@fankhausers.ch>
2018-12-14 23:25:35 +0100

    day 14, part 2

--- chocolate_charts/src/main.rs
+++ chocolate_charts/src/main.rs
@@ -28,37 +28,78 @@ fn main() {
     let input = input[0];
     println!("Input: {}", input);
 
-    let score = make_recipes([3, 7], input, 10);
+    let score = make_recipes_part1([3, 7], input, 10);
     println!("Score: {}", score);
+
+    let first_appearance = make_recipes_part2([3, 7], input);
+    println!("First appearance: {}", first_appearance);
 }
 
-fn make_recipes(
+fn make_recipes_part1(
     initial_state: [u8; 2],
     number_of_recipes: usize,
     recipes_in_score: usize,
 ) -> String {
+    make_recipes(initial_state, number_of_recipes, recipes_in_score, false).0
+}
+
+fn make_recipes_part2(initial_state: [u8; 2], number_of_recipes: usize) -> usize {
+    make_recipes(initial_state, number_of_recipes, 0, true).1
+}
+
+#[allow(clippy::mut_range_bound)]
+fn make_recipes(
+    initial_state: [u8; 2],
+    number_of_recipes: usize,
+    recipes_in_score: usize,
+    part2: bool,
+) -> (String, usize) {
     let mut positions = (0, 1);
     let mut state: Vec<u8> = vec![initial_state[0], initial_state[1]];
+    let mut idx = 0;
+    let mut idx_found = 0;
+    let sequence = number_of_recipes.to_string();
+    let mut recipes = "".to_string();
+    recipes.push((state[0] + b'0') as char);
+    recipes.push((state[1] + b'0') as char);
 
-    while state.len() < number_of_recipes + recipes_in_score {
+    while state.len() < number_of_recipes + recipes_in_score || (part2 && idx_found == 0) {
         let sum = state[positions.0] + state[positions.1];
         if sum > 9 {
-            state.push(sum / 10);
+            let digit = sum / 10;
+            state.push(digit);
+            recipes.push((digit + b'0') as char);
         }
-        state.push(sum - sum / 10 * 10);
+        let digit = sum - sum / 10 * 10;
+        state.push(digit);
+        recipes.push((digit + b'0') as char);
 
         positions = (
             (positions.0 + state[positions.0] as usize + 1) % state.len(),
             (positions.1 + state[positions.1] as usize + 1) % state.len(),
         );
+
+        if part2 && state.len() >= sequence.len() {
+            for i in idx..=(state.len() - sequence.len()) {
+                let recipes = &recipes[i..i + sequence.len()];
+                if recipes == sequence {
+                    idx_found = i;
+                    break;
+                }
+                idx += 1;
+            }
+        }
     }
 
-    state
-        .iter()
-        .skip(number_of_recipes)
-        .take(recipes_in_score)
-        .map(|&int| (int + b'0') as char)
-        .collect()
+    (
+        state
+            .iter()
+            .skip(number_of_recipes)
+            .take(recipes_in_score)
+            .map(|&int| (int + b'0') as char)
+            .collect(),
+        idx_found,
+    )
 }
 
 #[cfg(test)]
@@ -67,14 +108,21 @@ mod tests {
 
     #[test]
     fn test_initial_state() {
-        assert_eq!("37", make_recipes([3, 7], 0, 10));
+        assert_eq!("37", make_recipes_part1([3, 7], 0, 2));
     }
 
     #[test]
     fn test_recipes() {
-        assert_eq!("5158916779", make_recipes([3, 7], 9, 10));
-        assert_eq!("0124515891", make_recipes([3, 7], 5, 10));
-        assert_eq!("9251071085", make_recipes([3, 7], 18, 10));
-        assert_eq!("5941429882", make_recipes([3, 7], 2018, 10));
+        assert_eq!("5158916779", make_recipes_part1([3, 7], 9, 10));
+        assert_eq!("0124515891", make_recipes_part1([3, 7], 5, 10));
+        assert_eq!("9251071085", make_recipes_part1([3, 7], 18, 10));
+        assert_eq!("5941429882", make_recipes_part1([3, 7], 2018, 10));
+    }
+
+    #[test]
+    fn test_first_appearance() {
+        assert_eq!(9, make_recipes_part2([3, 7], 51589));
+        assert_eq!(18, make_recipes_part2([3, 7], 92510));
+        assert_eq!(2018, make_recipes_part2([3, 7], 59414));
     }
 }
