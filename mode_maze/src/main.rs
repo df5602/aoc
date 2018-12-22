43dc5ea7cee28036ab92b66207f22d8fba69a98e
df5602 <dominik@fankhausers.ch>
2018-12-22 09:36:50 +0100

    day 22, part 1

--- mode_maze/src/main.rs
+++ mode_maze/src/main.rs
@@ -37,4 +37,50 @@ fn main() {
 
     println!("depth = {}", depth);
     println!("target = ({},{})", target.0, target.1);
+
+    let grid = Grid::new(depth, target);
+    let total_risk_level = grid.total_risk_level();
+    println!("Total risk level: {}", total_risk_level);
+}
+
+struct Grid {
+    width: usize,
+    height: usize,
+    grid: Vec<usize>,
+}
+
+impl Grid {
+    fn new(depth: usize, target: (usize, usize)) -> Self {
+        let width = target.0 + 1;
+        let height = target.1 + 1;
+        let mut grid = Vec::with_capacity(width * height);
+
+        for y in 0..height {
+            for x in 0..width {
+                let gi = if (x == 0 && y == 0) || (x == target.0 && y == target.1) {
+                    0
+                } else if y == 0 {
+                    x * 16807
+                } else if x == 0 {
+                    y * 48271
+                } else {
+                    let el_left = grid[y * width + x - 1];
+                    let el_above = grid[(y - 1) * width + x];
+                    el_left * el_above
+                };
+                let el = (gi + depth) % 20183;
+                grid.push(el);
+            }
+        }
+
+        Self {
+            width,
+            height,
+            grid,
+        }
+    }
+
+    fn total_risk_level(&self) -> usize {
+        self.grid.iter().map(|el| el % 3).sum()
+    }
 }
