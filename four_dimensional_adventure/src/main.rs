b422b6222dd1a1e972bd5349ec106057dfa7940e
df5602 <dominik@fankhausers.ch>
2018-12-25 18:35:35 +0100

    day 25, part 1

--- four_dimensional_adventure/src/main.rs
+++ four_dimensional_adventure/src/main.rs
@@ -22,8 +22,95 @@ fn main() {
         }
     };
 
-    for point in input {
-        println!("{:?}", point);
+    let constellations = find_constellations(&input);
+    println!("Number of constellations: {}", constellations.len());
+}
+
+fn find_constellations(points: &[Point4D]) -> Vec<Constellation> {
+    let mut constellations: Vec<Constellation> = Vec::new();
+
+    for &point in points {
+        // Check whether part of existing constellations
+        let mut part_of_idx: Vec<usize> = Vec::new();
+
+        for (i, constellation) in constellations.iter().enumerate() {
+            if constellation.is_part_of(point) {
+                part_of_idx.push(i);
+            }
+        }
+
+        if part_of_idx.is_empty() {
+            // If point is not part of any constellation, create new constellation and add point
+            constellations.push(Constellation::new(point));
+        } else if part_of_idx.len() == 1 {
+            // If point is part of exactly one constellation, add to constellation
+            constellations[part_of_idx[0]].add_point(point);
+        } else {
+            // If point is part of multiple constellations, merge constellations and add point
+            let mut to_merge: Vec<Constellation> = Vec::new();
+
+            // Hack, to enable working with indices
+            for idx in part_of_idx {
+                to_merge.push(std::mem::replace(
+                    &mut constellations[idx],
+                    Constellation::empty(),
+                ));
+            }
+            constellations.retain(|constellation| !constellation.is_empty());
+
+            // One constellation will contain all points in merged constellation
+            let mut new_constellation = to_merge.pop().unwrap();
+
+            // Merge all other constellations into new one
+            for mut constellation in to_merge.iter_mut() {
+                new_constellation.merge(&mut constellation);
+            }
+
+            // Add new point to constellation
+            new_constellation.add_point(point);
+            constellations.push(new_constellation);
+        }
+    }
+
+    constellations
+}
+
+#[derive(Debug, Clone)]
+struct Constellation {
+    points: Vec<Point4D>,
+}
+
+impl Constellation {
+    fn new(point: Point4D) -> Self {
+        Self {
+            points: vec![point],
+        }
+    }
+
+    fn empty() -> Self {
+        Self { points: Vec::new() }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.points.is_empty()
+    }
+
+    fn is_part_of(&self, point: Point4D) -> bool {
+        for p in self.points.iter() {
+            if p.manhattan_distance_to(point) <= 3 {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn add_point(&mut self, point: Point4D) {
+        self.points.push(point);
+    }
+
+    fn merge(&mut self, other: &mut Constellation) {
+        self.points.append(&mut other.points);
     }
 }
 
@@ -35,16 +122,24 @@ struct Point4D {
     t: isize,
 }
 
+impl Point4D {
+    fn new(x: isize, y: isize, z: isize, t: isize) -> Self {
+        Self { x, y, z, t }
+    }
+
+    fn manhattan_distance_to(self, other: Point4D) -> usize {
+        (self.x - other.x).abs() as usize
+            + (self.y - other.y).abs() as usize
+            + (self.z - other.z).abs() as usize
+            + (self.t - other.t).abs() as usize
+    }
+}
+
 impl FromStr for Point4D {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut point = Point4D {
-            x: 0,
-            y: 0,
-            z: 0,
-            t: 0,
-        };
+        let mut point = Point4D::new(0, 0, 0, 0);
         for (i, coord) in s
             .split(',')
             .map(|s| s.trim())
