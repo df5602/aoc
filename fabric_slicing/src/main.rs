0d380a562a51640e082b85c917c86608b048d909
df5602 <dominik@fankhausers.ch>
2019-01-03 18:41:54 +0100

    refactor and add integration tests

--- fabric_slicing/src/main.rs
+++ fabric_slicing/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         }
     };
 
-    let input: Vec<Claim> = match FileReader::new().read_from_file(input_file) {
+    let claims: Vec<Claim> = match FileReader::new().read_from_file(input_file) {
         Ok(input) => input,
         Err(e) => {
             println!("Error reading input: {}", e);
@@ -23,25 +23,41 @@ fn main() {
         }
     };
 
-    let (max_width, max_height) = input
-        .iter()
-        .map(|claim| &claim.rectangle)
-        .map(|rect| (rect.x() + rect.width(), rect.y() + rect.height()))
-        .fold((0, 0), |m, dims| (max(m.0, dims.0), max(m.1, dims.1)));
-    let mut grid = Grid::new(max_width, max_height);
-
-    for claim in input.iter() {
-        grid.add_rectangle(&claim.rectangle);
-    }
+    let grid = create_grid(&claims);
 
     println!(
         "Square inches within two or more claims: {}",
         grid.count_eq_or_above(2)
     );
 
+    let non_overlapping_claim = find_non_overlapping_claim(&claims);
+
+    match non_overlapping_claim {
+        Some(id) => println!("Only non-overlapping claim: {}", id),
+        None => println!("No non-overlapping claim found!"),
+    }
+}
+
+fn create_grid(claims: &[Claim]) -> Grid {
+    let (max_width, max_height) = claims
+        .iter()
+        .map(|claim| &claim.rectangle)
+        .map(|rect| (rect.x() + rect.width(), rect.y() + rect.height()))
+        .fold((0, 0), |m, dims| (max(m.0, dims.0), max(m.1, dims.1)));
+    let mut grid = Grid::new(max_width, max_height);
+
+    for claim in claims.iter() {
+        grid.add_rectangle(&claim.rectangle);
+    }
+
+    grid
+}
+
+fn find_non_overlapping_claim(claims: &[Claim]) -> Option<usize> {
     let mut non_overlapping_claim = None;
-    for a in input.iter() {
-        if input
+
+    for a in claims.iter() {
+        if claims
             .iter()
             .filter(|&b| a.owner != b.owner)
             .all(|b| !a.rectangle.collides_with(&b.rectangle))
@@ -51,10 +67,7 @@ fn main() {
         }
     }
 
-    match non_overlapping_claim {
-        Some(id) => println!("Only non-overlapping claim: {}", id),
-        None => println!("No non-overlapping claim found!"),
-    }
+    non_overlapping_claim
 }
 
 #[derive(Debug, FromStr)]
@@ -99,5 +112,24 @@ impl Grid {
 
     fn count_eq_or_above(&self, val: u32) -> usize {
         self.grid.iter().filter(|&v| *v >= val).count()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let claims: Vec<Claim> = FileReader::new().read_from_file("input.txt").unwrap();
+        let grid = create_grid(&claims);
+        assert_eq!(101781, grid.count_eq_or_above(2));
+    }
+
+    #[test]
+    fn test_part2() {
+        let claims: Vec<Claim> = FileReader::new().read_from_file("input.txt").unwrap();
+        let non_overlapping_claim = find_non_overlapping_claim(&claims).unwrap();
+        assert_eq!(909, non_overlapping_claim);
     }
 }
